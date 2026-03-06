2488e66ce731e8afffb2ff3608b6fb25c9903781
Myriam MR. <myriammalikiyarukata@gmail.com>
2026-03-06 11:57:00 +0100

    Payment Service init

---
+++ PaymentService/Dtos/UpdatePaymentDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentService.Dtos;
+
+public class UpdatePaymentDto
+{
+    [Range(0.01, double.MaxValue)]
+    public decimal? Amount { get; set; }
+
+    public DateTime? PaymentDate { get; set; }
+
+    [StringLength(500)]
+    public string? Notes { get; set; }
+}
---
+++ PaymentService/Repository/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using PaymentService.Data;
+
+namespace PaymentService.Repository;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly AppDbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public Repository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+    public Task<List<T>> GetAllAsync()
+        => _dbSet.ToListAsync();
+
+    public Task<List<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        => _dbSet.Where(predicate).ToListAsync();
+
+    public Task<T?> GetByIdAsync(int id)
+        => _dbSet.FindAsync(id).AsTask();
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        return entity;
+    }
+
+    public Task UpdateAsync(T entity)
+    {
+        _dbSet.Update(entity);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(T entity)
+    {
+        _dbSet.Remove(entity);
+        return Task.CompletedTask;
+    }
+
+    public Task SaveChangesAsync()
+        => _context.SaveChangesAsync();
+}
